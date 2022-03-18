ab5955bfee435cfae2ed37894dd2be33c40849b8
wnata <wnatakillemall@gmail.com>
2022-03-18 16:52:06 +0200

    added page article dets

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ArticleDetailView
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail')
 ]

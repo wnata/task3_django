94bcd4b9df916ba2992683404f33ce5a739fff6d
wnata <wnatakillemall@gmail.com>
2022-03-21 12:35:54 +0200

    added web page for blog posts

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # def home(request):
@@ -13,3 +13,10 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_dets.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+    #fields = ('title', 'body')
